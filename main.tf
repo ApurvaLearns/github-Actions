7416e1aff84575d41f95947c37a1b76278859bf6
ApurvaLearns <57849941+ApurvaLearns@users.noreply.github.com>
2023-09-19 19:52:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 # Configure the AWS provider
 provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # Define your AWS resources here
